6cc57e07363406afe28c9b030d9d9e5e24e99c6f
TimNNyman <tim.n.nyman@gmail.com>
2018-04-17 14:42:36 +0200

    Working on publisher feed

---
+++ InteraktionsDesignPro2/PostData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace InteraktionsDesignPro2
+{
+    class PostData
+    {
+        public string Title;
+        public string Content;
+        public BitmapImage ImgSource { get; set; }
+
+        public PostData(string title, string content)
+        {
+            Title = title;
+            Content = content;
+            ImgSource = new BitmapImage();
+        }
+    }
+}
